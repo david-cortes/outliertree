2fbb127ee96c48074cabad4bcd1d324cb0aa7ef7
david-cortes <david.cortes.rivera@gmail.com>
2019-07-24 20:50:55 +0300

    CRAN policies

--- R/hypothyroid.R
+++ R/hypothyroid.R
@@ -2,6 +2,8 @@
 #'
 #' This data contains several obvious outliers from misspellings in data entry.
 #' From Garavan Institute. For more details see link at the bottom.
-#'
+#' 
+#' @docType data
+#' @usage data(hypothyroid)
 #' @source \url{http://archive.ics.uci.edu/ml/datasets/thyroid+disease}
 "hypothyroid"
